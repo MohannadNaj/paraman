851f6e1261c6eb2dc27899188ff331249dc06689
MohannadNaj <me@mohannad.io>
2017-11-05 04:58:21 +0300

    Views: Use only one view, use `paraman` component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,26 @@
-@extends('parameters::shared.parameters-layout')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('content')
-  <parameters ref="all-parameters" :parameters_list='{!! $parameters->toJson(JSON_HEX_APOS) !!}'></parameters>
-@endsection
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Styles -->
+    <link href="{{asset('vendor/parameters/css/app.css')}}" rel="stylesheet">
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode(Parameter\ParametersManager::clientData()); ?>;
+    </script>
+
+</head>
+<body>
+    <div class="container-fluid" id="app">
+        <paraman :parameters_list='{!! $parameters->toJson(JSON_HEX_APOS) !!}'></paraman>
+    </div>
+<script src="{{asset('vendor/parameters/js/app.js') . '?v='.Illuminate\Support\Str::random()}}"></script>
+</body>
+</html>
